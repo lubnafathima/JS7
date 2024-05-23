c3b15e0da6da970824c4b00a071edb5e0d4a17b1
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:50:33 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,103 @@
+// 1. Write a function that adds two numbers and returns the result.
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 5));
+
+// 2. Create a function that subtracts one number from another.
+function sub(a, b) {
+    return a - b;
+}
+console.log(sub(5, 3));
+
+// 3. Write a function that multiplies two numbers.
+function mul(a, b) {
+    return a * b;
+}
+console.log(mul(5, 3));
+
+// 4. Create a function that divides one number by another.
+function div(a, b) {
+    return a / b;
+}
+console.log(div(6, 3));
+
+// 5. Write a function that calculates the square of a number.
+function square(a) {
+    return a * a;
+}
+console.log(square(5));
+
+// 6. Create a function that calculates the cube of a number.
+function cube(a) {
+    return a * a;
+}
+console.log(cube(5));
+
+// 7. Write a function that concatenates two strings.
+function concat(str1, str2) {
+    return str1 + " " + str2;
+}
+console.log(concat("Jane", "Doe"));
+
+// 8. Create a function that checks if a number is even.
+function isEven(a) {
+    if(a % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    };
+}
+console.log(isEven(5));
+
+// 9. Write a function that checks if a number is prime.
+
+// 10. Create a function that converts Fahrenheit to Celsius.
+
+// 11. Write a function that converts Celsius to Fahrenheit.
+
+// 12. Create a function that finds the maximum of two numbers.
+function max(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(max(5, 2));
+
+// 13. Write a function that finds the minimum of two numbers.
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(min(5, 2));
+
+// 14. Create a function that calculates the average of numbers from 1 to 100.
+function avg(a, b) {
+    return Math.floor((a+b) / 2);
+}
+console.log(avg(1, 100));
+
+// 15. Write a function that checks if a number is positive, negative, or zero.
+function checkNumber(a) {
+    if (a > 0) {
+        return "positive";
+    } else if (a < 0) {
+        return "negative";
+    } else {
+        return "zero";
+    }
+}
+console.log(checkNumber(5));
+
+// 16. Write a function that finds the factorial of a number.
+
+// 17. Create a function that generates a random number between two given values.
+
+// 18. Write a function that checks if a given year is a leap year.
+
+// 19. Create a function that calculates the area of a rectangle.
